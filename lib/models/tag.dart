1b71c7de00d8901a46330d466aaa8ddca61d8812
naman-jn <namanjn1534@gmail.com>
2021-02-27 15:14:12 +0530

    minor changes

--- lib/models/tag.dart
+++ lib/models/tag.dart
@@ -1,17 +1,16 @@
 import 'package:document_organizer/utils/database_helper.dart';
 
-class Tag{
+class Tag {
   int _id;
   String _name;
   String _fileIds;
 
   DatabaseHelper databaseHelper = DatabaseHelper();
-  Tag.withID(this._id, this._name,  this._fileIds);
+  Tag.withID(this._id, this._name, this._fileIds);
 
-  Tag(this._name,this._fileIds);
+  Tag(this._name, this._fileIds);
 
   int get id => _id;
-
 
   String get name => _name;
 
@@ -25,11 +24,9 @@ class Tag{
     _fileIds = value;
   }
 
-  Map<String,dynamic> toMap() {
+  Map<String, dynamic> toMap() {
     return {
-
-      if(id != null)
-        'id': _id,
+      if (id != null) 'id': _id,
       'name': _name,
       'fileIds': _fileIds,
     };
@@ -41,42 +38,45 @@ class Tag{
     this._fileIds = map['fileIds'];
   }
 
-  List<String> toList(){
-    return _fileIds.substring(1,_fileIds.length-1).split(',');
+  List<String> toList() {
+    return _fileIds.substring(1, _fileIds.length - 1).split(',');
   }
 
-  void addFile(int fileId,Tag tag){
+  void addFile(int fileId, Tag tag) {
     //print(_fileIds);
-    List fileIdListX=_fileIds.substring(1,_fileIds.length-1).split(','); //'\\s*,\\s*'
-    List<int> fileIdList=List();
-    if(fileIdListX.first!='')
-    fileIdListX.forEach((element) {fileIdList.add(int.parse(element.trim()));});
-    if(fileIdListX.first!=''){
+    List fileIdListX =
+        _fileIds.substring(1, _fileIds.length - 1).split(','); //'\\s*,\\s*'
+    List<int> fileIdList = List();
+    if (fileIdListX.first != '')
+      fileIdListX.forEach((element) {
+        fileIdList.add(int.parse(element.trim()));
+      });
+    if (fileIdListX.first != '') {
       fileIdList.add(fileId);
       //print(fileIdList.length);
-      _fileIds=fileIdList.toSet().toList().toString();
-    }
-   else{
-     _fileIds='['+fileId.toString()+']';
+      _fileIds = fileIdList.toSet().toList().toString();
+    } else {
+      _fileIds = '[' + fileId.toString() + ']';
     }
     databaseHelper.updateTag(tag);
   }
 
-  void deleteFile(int fileId,Tag tag){
+  void deleteFile(int fileId, Tag tag) {
     //print(_fileIds);
-    List fileIdListX=_fileIds.substring(1,_fileIds.length-1).split(','); //'\\s*,\\s*'
-    List<int> fileIdList=List();
-    if(fileIdListX.first!='')
-      fileIdListX.forEach((element) {fileIdList.add(int.parse(element.trim()));});
-    if(fileIdListX.first!=''){
+    List fileIdListX =
+        _fileIds.substring(1, _fileIds.length - 1).split(','); //'\\s*,\\s*'
+    List<int> fileIdList = List();
+    if (fileIdListX.first != '')
+      fileIdListX.forEach((element) {
+        fileIdList.add(int.parse(element.trim()));
+      });
+    if (fileIdListX.first != '') {
       fileIdList.remove(fileId);
       //print(fileIdList.length);
-      _fileIds=fileIdList.toSet().toList().toString();
-    }
-    else{
-      _fileIds='['+fileId.toString()+']';
+      _fileIds = fileIdList.toSet().toList().toString();
+    } else {
+      _fileIds = '[' + fileId.toString() + ']';
     }
     databaseHelper.updateTag(tag);
   }
-
 }
